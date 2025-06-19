f52eda08b1cd1cc18b7c9936cf690e4c6653d6cb
DmitriiBank <dm.skakov@gmail.com>
2025-06-19 10:20:20 +0300

    Add gh-pages

--- src/utils/shop-types.ts
+++ src/utils/shop-types.ts
@@ -1,13 +1,13 @@
 export enum Paths {
     HOME = '/',
-    ORDERS = 'orders',
-    PRODUCTS = 'products',
-    CART = 'cart',
-    CUSTOMERS = 'customers',
-    BREAD = 'bread',
-    DAIRY = 'dairy',
+    ORDERS = '/orders',
+    PRODUCTS = '/products',
+    CART = '/cart',
+    CUSTOMERS = '/customers',
+    BREAD = '/bread',
+    DAIRY = '/dairy',
     ERROR = '/error',
-    BACK = 'back',
+    BACK = '/back',
     LOGIN = '/login',
 }
 
